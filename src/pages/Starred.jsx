cd5862afefd071165d5b8673d2445c017f6b92d7
Hp <Hp@DESKTOP-8HIKA16.(none)>
2023-06-29 10:23:38 +0530

    featch starred shows

--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -1,6 +1,25 @@
+import { useQuery } from '@tanstack/react-query';
 import { useStarredShows } from '../lib/useStarredShows';
+import { getShowByIds } from '../api/tvmaze';
+import ShowGrid from '../components/shows/ShowGrid';
 const Starred = () => {
-  const [starredShows] = useStarredShows();
-  return <div>Starred page, starred {starredShows.length}</div>;
+  const [starredShowsIds] = useStarredShows();
+
+  const { data: starredShows, error: starredShowsError } = useQuery({
+    queryKey: ['starred', starredShowsIds],
+    queryFn: async () =>
+      getShowByIds(starredShowsIds).then(result =>
+        result.map(show => ({ show }))
+      ),
+    refetchOnWindowFocus: false,
+  });
+  if (starredShows?.length === 0) {
+    return <div>No shows were starred</div>;
+  }
+  if (starredShows?.length > 0) {
+    return <ShowGrid shows={starredShows} />;
+  }
+
+  return <div>Starred page, starred {starredShowsIds.length}</div>;
 };
 export default Starred;
