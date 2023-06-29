f064d59c482d7742d715fd5225e8619113989dbc
Hp <Hp@DESKTOP-8HIKA16.(none)>
2023-06-29 16:00:11 +0530

    added icons and update links

--- src/theme.jsx
+++ src/theme.jsx
@@ -3,9 +3,9 @@ import { ThemeProvider, createGlobalStyle } from 'styled-components';
 const theme = {
   fontFamily: 'Roboto, sans-serif',
   mainColors: {
-    blue: '#2400ff',
+    blue: '#5e4dd1',
     gray: '#c6c6c6',
-    dark: '#353535',
+    dark: '#3b3131',
   },
 };
 const GlobalStyles = createGlobalStyle`
